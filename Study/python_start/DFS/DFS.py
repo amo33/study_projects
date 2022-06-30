2bacbccee6a62d95b4028e7b1ac1ba45eb674e26
amo33 <andylee70044@gmail.com>
2022-06-30 20:26:18 +0900

    dfs commit

--- Study/python_start/DFS/DFS.py
+++ Study/python_start/DFS/DFS.py
@@ -1,13 +1,33 @@
+import sys 
 
-def DFS(startnode,graph):
-    need_visit , visited = [], []
-
-    need_visit.append(startnode)
-
-    while(need_visit):
-        node = need_visit.pop() 
-        if node not in visited: # list index = integer or slice 
-            visited.append(node)
-            need_visit.extend(graph[node])
-
-    return visited   
+N = int(input())
+M = int(input())
+visited = [0 for _ in range(N+1)]
+tracked = []
+map = []
+stack =[]
+stack = []
+start = 1
+stack.append(start)
+tracked.append(start)
+def dfs(stack):
+    while stack != []:
+        val = stack.pop()
+        for i in range(1,N+1):
+            if i != val:
+                if map[val][i] == 1 and visited[i]== 0:
+                    tracked.append(i)
+                    stack.append(i)
+for i in range(N+1):
+    map = [0 for _ in range(N+1)]
+for i in range(M):
+    start, end = map(int, input().split())
+    map[start][end] = 1
+    map[end][start] = 1
+dfs(stack)
+print(tracked)
+# 1 2
+# 1 3
+# 1 4
+# 2 4
+# 3 4
